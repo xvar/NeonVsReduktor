031458cd8dd9286c957caf76e825345fb2094a01
Alexey Khaiminov <a.khaiminov@corp.mail.ru>
2021-04-21 19:56:30 +0300

    initial commit

---
+++ app/src/main/java/com/github/xvar/neon/reduktor/ui/activity/LaunchActivity.kt
@@ -0,0 +1,14 @@
+package com.github.xvar.neon.reduktor.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.xvar.neon.reduktor.R
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+
+}
